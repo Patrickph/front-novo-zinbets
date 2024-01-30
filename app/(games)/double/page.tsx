69f2d9ebdd2aed9dc492738ce593463bde689cb5
Anderson Vieira <andervieira1995@gmail.com>
2024-01-30 19:23:49 -0300

    change double page

--- app/(games)/double/page.tsx
+++ app/(games)/double/page.tsx
@@ -204,135 +204,134 @@ export default function DoublePage() {
   }, [game?.status]);
 
   return (
-    <>Jogo em manutenção</>
-    // <>
-    //   <div className="rounded bg-zinc-700 p-6 flex flex-col gap-6">
-    //     <div className="grid grid-cols-3 gap-4">
-    //       <form
-    //         onSubmit={handleSubmit(onSubmit)}
-    //         className="border-t pt-4 lg:pt-0 lg:border-t-0 lg:border-r lg:pr-3 border-zinc-600 order-last lg:order-first col-span-3 lg:col-span-1"
-    //       >
-    //         <Input
-    //           title="Valor da Aposta"
-    //           type="tel"
-    //           disabled={isBet}
-    //           currency={true}
-    //           {...register("value_to_bet", { required: true })}
-    //           errors={errors.value_to_bet?.message}
-    //         />
+    <>
+      <div className="rounded bg-zinc-700 p-6 flex flex-col gap-6">
+        <div className="grid grid-cols-3 gap-4">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="border-t pt-4 lg:pt-0 lg:border-t-0 lg:border-r lg:pr-3 border-zinc-600 order-last lg:order-first col-span-3 lg:col-span-1"
+          >
+            <Input
+              title="Valor da Aposta"
+              type="tel"
+              disabled={isBet}
+              currency={true}
+              {...register("value_to_bet", { required: true })}
+              errors={errors.value_to_bet?.message}
+            />
 
-    //         <div className="flex -mt-2 mb-4 items-center justify-between rounded overflow-hidden">
-    //           <button
-    //             type="button"
-    //             disabled={isBet}
-    //             onClick={half_value_to_bet}
-    //             className="disabled:cursor-not-allowed disabled:hover:bg-zinc-800 disabled:hover:text-zinc-400 h-10 text-sm flex justify-center items-center cursor-pointer font-semibold bg-zinc-800 hover:text-zinc-950 hover:bg-green-500 flex-1 text-center text-zinc-400"
-    //           >
-    //             1/2
-    //           </button>
-    //           <button
-    //             type="button"
-    //             disabled={isBet}
-    //             onClick={double_value_to_bet}
-    //             className="disabled:cursor-not-allowed disabled:hover:bg-zinc-800 disabled:hover:text-zinc-400 h-10 text-sm flex justify-center items-center cursor-pointer font-semibold bg-zinc-800 hover:text-zinc-950 hover:bg-green-500 flex-1 text-center text-zinc-400"
-    //           >
-    //             2x
-    //           </button>
-    //           <button
-    //             type="button"
-    //             disabled={isBet}
-    //             onClick={max_value_to_bet}
-    //             className="disabled:cursor-not-allowed disabled:hover:bg-zinc-800 disabled:hover:text-zinc-400 h-10 text-sm flex justify-center items-center cursor-pointer font-semibold bg-zinc-800 hover:text-zinc-950 hover:bg-green-500 flex-1 text-center text-zinc-400"
-    //           >
-    //             MAX
-    //           </button>
-    //         </div>
+            <div className="flex -mt-2 mb-4 items-center justify-between rounded overflow-hidden">
+              <button
+                type="button"
+                disabled={isBet}
+                onClick={half_value_to_bet}
+                className="disabled:cursor-not-allowed disabled:hover:bg-zinc-800 disabled:hover:text-zinc-400 h-10 text-sm flex justify-center items-center cursor-pointer font-semibold bg-zinc-800 hover:text-zinc-950 hover:bg-green-500 flex-1 text-center text-zinc-400"
+              >
+                1/2
+              </button>
+              <button
+                type="button"
+                disabled={isBet}
+                onClick={double_value_to_bet}
+                className="disabled:cursor-not-allowed disabled:hover:bg-zinc-800 disabled:hover:text-zinc-400 h-10 text-sm flex justify-center items-center cursor-pointer font-semibold bg-zinc-800 hover:text-zinc-950 hover:bg-green-500 flex-1 text-center text-zinc-400"
+              >
+                2x
+              </button>
+              <button
+                type="button"
+                disabled={isBet}
+                onClick={max_value_to_bet}
+                className="disabled:cursor-not-allowed disabled:hover:bg-zinc-800 disabled:hover:text-zinc-400 h-10 text-sm flex justify-center items-center cursor-pointer font-semibold bg-zinc-800 hover:text-zinc-950 hover:bg-green-500 flex-1 text-center text-zinc-400"
+              >
+                MAX
+              </button>
+            </div>
 
-    //         <div className="w-full flex gap-2 mb-4">
-    //           <button
-    //             type="button"
-    //             className={`
-    //             ${watch("color") == "black" ? "border-4 border-red-500" : ""}
-    //             cursor-pointer flex-1 h-12 rounded-lg font-semibold bg-black flex items-center justify-center`}
-    //             onClick={() => setValue("color", "black")}
-    //           >
-    //             x2
-    //           </button>
-    //           <button
-    //             type="button"
-    //             className={`
-    //             ${watch("color") == "white" ? "border-4 border-red-500" : ""}
-    //             cursor-pointer flex-1 h-12 rounded-lg font-semibold text-zinc-700 bg-white flex items-center justify-center`}
-    //             onClick={() => setValue("color", "white")}
-    //           >
-    //             x14
-    //           </button>
-    //           <button
-    //             type="button"
-    //             className={`
-    //             ${watch("color") == "green" ? "border-4 border-red-500" : ""}
-    //             cursor-pointer flex-1 h-12 rounded-lg font-semibold bg-green-500 text-zinc-700 flex items-center justify-center`}
-    //             onClick={() => setValue("color", "green")}
-    //           >
-    //             x2
-    //           </button>
-    //         </div>
-    //         {errors.color && (
-    //           <BadgeErrorsMessage
-    //             type={"error"}
-    //             message={"Selecione uma cor antes de apostar"}
-    //           />
-    //         )}
+            <div className="w-full flex gap-2 mb-4">
+              <button
+                type="button"
+                className={`
+                ${watch("color") == "black" ? "border-4 border-red-500" : ""}
+                cursor-pointer flex-1 h-12 rounded-lg font-semibold bg-black flex items-center justify-center`}
+                onClick={() => setValue("color", "black")}
+              >
+                x2
+              </button>
+              <button
+                type="button"
+                className={`
+                ${watch("color") == "white" ? "border-4 border-red-500" : ""}
+                cursor-pointer flex-1 h-12 rounded-lg font-semibold text-zinc-700 bg-white flex items-center justify-center`}
+                onClick={() => setValue("color", "white")}
+              >
+                x14
+              </button>
+              <button
+                type="button"
+                className={`
+                ${watch("color") == "green" ? "border-4 border-red-500" : ""}
+                cursor-pointer flex-1 h-12 rounded-lg font-semibold bg-green-500 text-zinc-700 flex items-center justify-center`}
+                onClick={() => setValue("color", "green")}
+              >
+                x2
+              </button>
+            </div>
+            {errors.color && (
+              <BadgeErrorsMessage
+                type={"error"}
+                message={"Selecione uma cor antes de apostar"}
+              />
+            )}
 
-    //         <BadgeErrorsMessage
-    //           type={messageError.type}
-    //           message={messageError.message}
-    //         />
+            <BadgeErrorsMessage
+              type={messageError.type}
+              message={messageError.message}
+            />
 
-    //         {!isBet ? (
-    //           waitBet > 0 ? (
-    //             <div
-    //               onClick={handleResetWaitBet}
-    //               className="h-12 flex items-center text-center w-full justify-center cursor-pointer  rounded shadow-green shadow-red-500 bg-red-500 text-white hover:bg-red-700 px-2 py-2  font-bold text-xs md:text-sm"
-    //             >
-    //               Na fila (clique para cancelar)
-    //             </div>
-    //           ) : (
-    //             <button
-    //               type="submit"
-    //               className="h-12 w-full text-center cursor-pointer  rounded shadow-green shadow-green-500 bg-green-500 text-zinc-900 hover:bg-green-700 px-2 py-2  font-bold text-xs md:text-sm"
-    //             >
-    //               Começar o jogo
-    //             </button>
-    //           )
-    //         ) : (
-    //           <button
-    //             type="button"
-    //             className="disabled:cursor-not-allowed disabled:opacity-80 h-12 w-full text-center cursor-pointer  rounded shadow-green shadow-red-500 bg-red-500 text-white hover:bg-red-700 px-2 py-2  font-bold text-xs md:text-sm"
-    //           >
-    //             Esperando
-    //           </button>
-    //         )}
-    //       </form>
+            {!isBet ? (
+              waitBet > 0 ? (
+                <div
+                  onClick={handleResetWaitBet}
+                  className="h-12 flex items-center text-center w-full justify-center cursor-pointer  rounded shadow-green shadow-red-500 bg-red-500 text-white hover:bg-red-700 px-2 py-2  font-bold text-xs md:text-sm"
+                >
+                  Na fila (clique para cancelar)
+                </div>
+              ) : (
+                <button
+                  type="submit"
+                  className="h-12 w-full text-center cursor-pointer  rounded shadow-green shadow-green-500 bg-green-500 text-zinc-900 hover:bg-green-700 px-2 py-2  font-bold text-xs md:text-sm"
+                >
+                  Começar o jogo
+                </button>
+              )
+            ) : (
+              <button
+                type="button"
+                className="disabled:cursor-not-allowed disabled:opacity-80 h-12 w-full text-center cursor-pointer  rounded shadow-green shadow-red-500 bg-red-500 text-white hover:bg-red-700 px-2 py-2  font-bold text-xs md:text-sm"
+              >
+                Esperando
+              </button>
+            )}
+          </form>
 
-    //       <div className="col-span-3 lg:col-span-2 flex flex-col gap-4 ">
-    //         <div className="block lg:hidden w-full">
-    //           <LastGamesDouble />
-    //         </div>
+          <div className="col-span-3 lg:col-span-2 flex flex-col gap-4 ">
+            <div className="block lg:hidden w-full">
+              <LastGamesDouble />
+            </div>
 
-    //         <div className="w-full relative bg-zinc-900 rounded-xl overflow-hidden py-6">
-    //           <ResultDoubleMessage />
-    //           <SliderDouble />
-    //         </div>
+            <div className="w-full relative bg-zinc-900 rounded-xl overflow-hidden py-6">
+              <ResultDoubleMessage />
+              <SliderDouble />
+            </div>
 
-    //         <div className="hidden lg:block w-full border-t border-zinc-500 pt-4">
-    //           <LastGamesDouble />
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
+            <div className="hidden lg:block w-full border-t border-zinc-500 pt-4">
+              <LastGamesDouble />
+            </div>
+          </div>
+        </div>
+      </div>
 
-    //   <ListBetsMines />
-    // </>
+      <ListBetsMines />
+    </>
   );
 }
