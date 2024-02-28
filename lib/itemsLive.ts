9fbace1587d2eea30cd31812b01b48be39c03325
joseVitorDev <87384654+joseVitorDev@users.noreply.github.com>
2024-02-28 20:02:06 -0300

    Arrumar jogos ao vivo

--- lib/itemsLive.ts
+++ lib/itemsLive.ts
@@ -4,216 +4,216 @@ export const itemsLive = [
     name: "XXXtreme Lightning Roulette",
     href: "/game/XxxtremeLigh0001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/XxxtremeLigh0001.webp`,
-  
+    active: false,
   },
   {
     key: 805,
     name: "Bac Bo",
     href: "/game/BacBo00000000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/BacBo00000000001.webp`,
-  
+  active: false,
   },
   {
     key: 831,
     name: "Crazy Time",
     href: "/game/CrazyTime0000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/CrazyTime0000001.webp`,
-  
+  active: false,
   },
   {
     key: 845,
     name: "Football Studio",
     href: "/game/TopCard000000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/TopCard000000001.webp`,
-  
+  active: false,
   },
   {
     key: 820,
     name: "Auto-Roulette",
     href: "/game/8z5pjps3ntvqc1b",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/8z5pjps3ntvqc1b.webp`,
-  
+  active: false,
   },
   {
     key: 846,
     name: "Immersive Roulette",
     href: "/game/7x0b1tgh7agmf6hv",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/7x0b1tgh7agmf6hv.webp`,
-  
+  active: false,
   },
   {
     key: 853,
     name: "Auto-Roulette VIP",
     href: "/game/01rb77cq1gtenhmo",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/01rb77cq1gtenhmo.webp`,
-  
+  active: false,
   },
   {
     key: 855,
     name: "Speed Roulette",
     href: "/game/lkcbrbdckjxajdol",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/lkcbrbdckjxajdol.webp`,
-  
+  active: false,
   },
   {
     key: 808,
     name: "Football Studio Dice",
     href: "/game/TopDice000000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/TopDice000000001.webp`,
-  
+  active: false,
   },
   {
     key: 830,
     name: "Lightning Roulette",
     href: "/game/LightningTable01",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/LightningTable01.webp`,
-  
+  active: false,
   },
   {
     key: 841,
     name: "Football Studio Roulette",
     href: "/game/7nyiaws9tgqrzaz3",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/7nyiaws9tgqrzaz3.webp`,
-  
+  active: false,
   },
   {
     key: 809,
     name: "Monopoly Live",
     href: "/game/Monopoly00000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/Monopoly00000001.webp`,
-  
+  active: false,
   },
   {
     key: 867,
     name: "Fan Tan",
     href: "/game/FanTan0000000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/FanTan0000000001.webp`,
-  
+  active: false,
   },
   {
     key: 822,
     name: "Gold Vault Roulette",
     href: "/game/GoldVaultRo00001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/GoldVaultRo00001.webp`,
-  
+  active: false,
   },
   {
     key: 816,
     name: "Dragon Tiger",
     href: "/game/DragonTiger00001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/DragonTiger00001.webp`,
-  
+  active: false,
   },
   {
     key: 874,
     name: "Monopoly Big Baller",
     href: "/game/MonBigBaller0001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/MonBigBaller0001.webp`,
-  
+  active: false,
   },
   {
     key: 834,
     name: "Teen Patti",
     href: "/game/teenpattitable01",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/teenpattitable01.webp`,
-  
+  active: false,
   },
   {
     key: 856,
     name: "American Roulette",
     href: "/game/AmericanTable001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/AmericanTable001.webp`,
-  
+  active: false,
   },
   {
     key: 863,
     name: "Baccarat",
     href: "/game/oytmvb9m1zysmc44",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/oytmvb9m1zysmc44.webp`,
-  
+  active: false,
   },
   {
     key: 807,
     name: "Grand Casino Roulette",
     href: "/game/lr6t4k3lcd4qgyrk",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/lr6t4k3lcd4qgyrk.webp`,
-  
+  active: false,
   },
   {
     key: 802,
     name: "Double Ball Roulette",
     href: "/game/DoubleBallRou001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/DoubleBallRou001.webp`,
-  
+  active: false,
   },
   {
     key: 824,
     name: "Triple Card Poker",
     href: "/game/TRPTable00000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/TRPTable00000001.webp`,
-  
+  active: false,
   },
   {
     key: 876,
     name: "Auto-Roulette La Partage",
     href: "/game/f1f4rm9xgh4j3u2z",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/f1f4rm9xgh4j3u2z.webp`,
-  
+  active: false,
   },
   {
     key: 802,
     name: "Prosperity Tree Baccarat",
     href: "/game/PTBaccarat000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/PTBaccarat000001.webp`,
-  
+  active: false,
   },
   {
     key: 849,
     name: "Roulette",
     href: "/game/vctlz20yfnmp1ylr",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/vctlz20yfnmp1ylr.webp`,
-  
+  active: false,
   },
   {
     key: 868,
     name: "Caribbean Stud Poker",
     href: "/game/CSPTable00000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/CSPTable00000001.webp`,
-  
+  active: false,
   },
   {
     key: 869,
     name: "Super Sic Bo",
     href: "/game/SuperSicBo000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/SuperSicBo000001.webp`,
-  
+  active: false,
   },
   {
     key: 865,
     name: "Lightning Baccarat",
     href: "/game/LightningBac0001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/LightningBac0001.webp`,
-  
+  active: false,
   },
   {
     key: 871,
     name: "Dream Catcher",
     href: "/game/MOWDream00000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/MOWDream00000001.webp`,
-  
+  active: false,
   },
   {
     key: 873,
     name: "Lightning Dice",
     href: "/game/LightningDice001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/LightningDice001.webp`,
-  
+  active: false,
   },
   {
     key: 873,
     name: "Extreme Texas Hold'em",
     href: "/game/ETHTable00000001",
     src: `/${process.env.NEXT_PUBLIC_SITE_NAME}/games/live/ETHTable00000001.webp`,
-  
+  active: false,
   },
 ]
